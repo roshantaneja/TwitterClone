b60814019ac2b352efde0dd7e96beb1d760f82ee
Daroshi11260 <rytaneja@gmail.com>
2022-05-14 20:32:46 -0700

    added bootstrap and CDNs, added Css Stylesheet for login page

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const middleware = require("./middleware")
+const path = require('path')
 
 const server = app.listen(port, () => console.log("server listening on port: " + port));
 
 app.set("view engine", "pug");
 app.set("veiws", "veiws");
+app.use(express.static(path.join(__dirname, "public")))
+
+//routes
+const loginRoute = require('./routes/loginRoutes');
+
+app.use("/login", loginRoute);
+
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
