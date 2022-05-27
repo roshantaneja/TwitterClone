650a56586b12a6f1095c5911e805375304a3e511
Yoshi <rytaneja@gmail.com>
2022-05-26 22:57:01 -0700

    code to recieve form submissions for register page

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -13,8 +13,22 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-    console.log(req.body)
-    res.status(200).render("register");
+    var firstName = req.body.firstName.trim();
+    var lastName = req.body.lastName.trim();
+    var username = req.body.username.trim();
+    var email = req.body.email.trim();
+    var password = req.body.password;
+
+    var payload = req.body;
+
+    if(firstName && lastName && username && email && password) {
+        
+    } else {
+        payload.errorMessage = "Make sure each field has a valid value!"
+        res.status(200).render("register", payload);
+    }
+
+    
 });
 
 module.exports = router;
